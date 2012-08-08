ace41eed5e20e84663747179ff78348b8c8afaf7
Matthew Casperson <mcaspers@redhat.com>
2012-08-08 14:07:50 -0700

    Waiting for a compile...

--- src/main/java/org/jboss/pressgangccms/client/local/ui/editor/SearchUIProjectsEditor.java
+++ src/main/java/org/jboss/pressgangccms/client/local/ui/editor/SearchUIProjectsEditor.java
@@ -6,13 +6,17 @@ import org.jboss.pressgangccms.client.local.ui.search.SearchUIProjects;
 import com.google.gwt.editor.client.Editor;
 import com.google.gwt.editor.client.adapters.EditorSource;
 import com.google.gwt.editor.client.adapters.ListEditor;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Grid;
 
 public class SearchUIProjectsEditor extends Grid implements Editor<SearchUIProjects>
 {
 	/**
-	 * The EditorSource is used to create and orgainse the Editors that go into a ListEditor
+	 * The EditorSource is used to create and orgainse the Editors that go into
+	 * a ListEditor
+	 * 
 	 * @author Matthew Casperson
 	 */
 	private class SearchUIProjectEditorSource extends EditorSource<SearchUIProjectEditor>
@@ -22,6 +26,16 @@ public class SearchUIProjectsEditor extends Grid implements Editor<SearchUIProje
 		{
 			final SearchUIProjectEditor subEditor = new SearchUIProjectEditor();
 			projectButtonPanel.setWidget(index, 0, subEditor.name);
+
+			subEditor.name.addClickHandler(new ClickHandler()
+			{
+				@Override
+				public void onClick(final ClickEvent event)
+				{
+					SearchUIProjectsEditor.this.setWidget(0, 1, subEditor);
+				}
+			});
+
 			return subEditor;
 		}
 
