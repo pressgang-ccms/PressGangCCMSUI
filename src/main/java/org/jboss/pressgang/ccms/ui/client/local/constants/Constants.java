e600f82a1c532dc98f5e096b4631f93ca877509d
mcasperson <matthewcasperson@gmail.com>
2012-11-12 13:53:58 +1000

    Updated version

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -10,7 +10,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm
      */
-    public static final String VERSION = "201211120817";
+    public static final String VERSION = "201211121353";
     
     /**
      * The sort order to apply to a newly added child
