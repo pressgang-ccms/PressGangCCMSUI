a978c3de610eb1080806ba79bb78eed0b1fe5258
mcasperson <matthewcasperson@gmail.com>
2012-11-14 08:26:19 +1000

    Moved to GWT 2.5.0

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -10,7 +10,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm
      */
-    public static final String VERSION = "201211131127";
+    public static final String VERSION = "201211140817";
     
     /**
      * The sort order to apply to a newly added child
