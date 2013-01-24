e1ff41b2025d4f529a0609a1294dd2ebf5fce72d
Matthew Casperson <matthewcasperson@gmail.com>
2013-01-24 12:45:32 +1000

    Removed topic search results and edit component

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -10,7 +10,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm
      */
-    public static final String VERSION = "201301231333";
+    public static final String VERSION = "201301241208";
     
     /**
      * The sort order to apply to a newly added child
