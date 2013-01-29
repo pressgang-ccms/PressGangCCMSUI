87bab7fd4d3cc19aa10780385ee832e402260838
Matthew Casperson <matthewcasperson@gmail.com>
2013-01-29 12:57:00 +1000

    Added some code comments

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/mvp/view/base/editor/BaseEditorViewInterface.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/mvp/view/base/editor/BaseEditorViewInterface.java
@@ -9,7 +9,12 @@ import com.google.gwt.editor.client.SimpleBeanEditorDriver;
  * This interface defines the methods that need top be found on views that use editors to display an entities details.
  * 
  * @author Matthew Casperson
+ * @param <W> The type of the object being bound to by the editor
+ * @param <X> The type of the editor that is bound to W
  */
 public interface BaseEditorViewInterface <W, X extends Editor<W>> extends BaseTemplateViewInterface {
+    /**
+     * @return The GWT editor that binds a POJO to a collection of UI elements.
+     */
     SimpleBeanEditorDriver<W, X> getDriver();
 }
