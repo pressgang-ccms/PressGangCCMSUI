119c57fbfe5f1649fed3806a641b542125f996ab
Matthew Casperson <matthewcasperson@gmail.com>
2013-02-28 14:02:07 +1000

    Working on category logic ui

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/restcalls/RESTCalls.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/restcalls/RESTCalls.java
@@ -82,8 +82,8 @@ public final class RESTCalls {
             "{\"branches\":[" +
                 "{\"trunk\":{\"name\": \"" + RESTFilterV1.FILTER_CATEGORIES_NAME + "\"}, \"branches\":[" +
                     "{\"trunk\":{\"name\": \"" + RESTFilterCategoryV1.CATEGORY_NAME + "\"}}," +
-                    "{\"trunk\":{\"name\": \"" + RESTFilterCategoryV1.PROJECT_NAME + "\"}}," +
-                "]}" +
+                    "{\"trunk\":{\"name\": \"" + RESTFilterCategoryV1.PROJECT_NAME + "\"}}" +
+                "]}," +
                 "{\"trunk\":{\"name\": \"" + RESTFilterV1.FILTER_FIELDS_NAME + "\"}}," +
                 "{\"trunk\":{\"name\": \"" + RESTFilterV1.FILTER_LOCALES_NAME + "\"}}," +
                 "{\"trunk\":{\"name\": \"" + RESTFilterV1.FILTER_TAGS_NAME + "\"}, \"branches\":[" +
