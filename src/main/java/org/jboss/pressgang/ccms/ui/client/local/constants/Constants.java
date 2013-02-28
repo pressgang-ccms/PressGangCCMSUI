20d064555f99380fbe5454d39ae6f412e8a41ffa
Matthew Casperson <matthewcasperson@gmail.com>
2013-02-28 10:40:57 +1000

    Working on category logic ui

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201302281032";
+    public static final String VERSION = "201302281037";
 
     /**
      * Indicates that a tag should be present in the returned topics
