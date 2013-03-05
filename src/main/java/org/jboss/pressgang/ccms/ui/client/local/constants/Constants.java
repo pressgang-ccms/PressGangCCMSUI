c4200139b43a68d9edc0821625759da0f0cc8638
Matthew Casperson <matthewcasperson@gmail.com>
2013-03-06 08:58:28 +1000

    Working on action button scrolling

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201303060822";
+    public static final String VERSION = "201303060845";
 
     /**
      * The sort order to apply to a newly added child.
