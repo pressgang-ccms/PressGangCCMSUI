67892658b91e29b30b3b2f36a1cb6fccc0e58f79
Matthew Casperson <matthewcasperson@gmail.com>
2013-03-07 12:49:25 +1000

    Added support for the new content spec rest interface

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201303070918";
+    public static final String VERSION = "201303071246";
 
     /**
      * The sort order to apply to a newly added child.
