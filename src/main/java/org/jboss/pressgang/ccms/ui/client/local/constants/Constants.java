c803676da7df0723331d745e6ed0db3f498d5c03
Matthew Casperson <matthewcasperson@gmail.com>
2013-04-17 13:54:29 +1000

    Working on adding content spec views

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201304171230";
+    public static final String VERSION = "201304171352";
 
     /**
      * The maximum length of the title before it is truncated
--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/restcalls/RESTCalls.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/restcalls/RESTCalls.java
@@ -70,7 +70,7 @@ public final class RESTCalls {
     /**
      * The required expansion details for the content specs.
      */
-    private static final String CONTENT_SPEC_EXPANSION = "{\"trunk\":{\"name\": \"" + RESTContentSpecV1.NODES_NAME + "\"}}";
+    private static final String CONTENT_SPEC_EXPANSION = "{\"trunk\":{\"name\": \"" + RESTContentSpecV1.CHILDREN_NAME + "\"}}";
     /**
      * The required expansion details for a topic. This is used when loading a topic for the first time
      */
