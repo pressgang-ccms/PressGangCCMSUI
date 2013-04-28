19367ef4d6d89e4217bee5aae04a02d3a5f0d92f
Matthew Casperson <matthewcasperson@gmail.com>
2013-04-29 09:37:23 +1000

    Added link to ATOM feed

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/mvp/presenter/topic/searchresults/topics/TopicFilteredResultsAndDetailsPresenter.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/mvp/presenter/topic/searchresults/topics/TopicFilteredResultsAndDetailsPresenter.java
@@ -1193,7 +1193,7 @@ public class TopicFilteredResultsAndDetailsPresenter extends BaseTopicFilteredRe
             searchResultsComponent.getDisplay().getAtomFeed().addClickHandler(new ClickHandler() {
                 @Override
                 public void onClick(@NotNull final ClickEvent event) {
-                    Window.open(Constants.REST_SERVER + "/1/topics/get/atom/" + getQueryString(), "", "");
+                    Window.open(Constants.REST_SERVER + "/1/topics/get/atom/" + getQueryString() + "?expand=%7B%22branches%22%3A%5B%7B%22trunk%22%3A%7B%22name%22%3A%20%22topics%22%7D%7D%5D%7D", "", "");
                 }
             });
 
