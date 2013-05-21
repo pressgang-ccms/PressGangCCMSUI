7678233dcaddeb8d54a0efdb0839dbfc0a55561f
Matthew Casperson <matthewcasperson@gmail.com>
2013-05-22 06:51:48 +1000

    Updated code to reflect new REST endpoint

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201305220647";
+    public static final String VERSION = "201305220651";
 
     /**
      * The maximum length of the title before it is truncated
