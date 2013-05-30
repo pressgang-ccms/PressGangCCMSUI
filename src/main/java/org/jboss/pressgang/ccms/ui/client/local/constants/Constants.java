2da5134a8969312c650460f4ba6b5bd964f0f720
Matthew Casperson <matthewcasperson@gmail.com>
2013-05-31 07:24:05 +1000

    Added some new words to the dictionary

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201305301554";
+    public static final String VERSION = "201305310646";
 
     /**
      * The maximum length of the title before it is truncated
