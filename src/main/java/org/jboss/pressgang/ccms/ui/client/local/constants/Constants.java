5bf3c1bf415ba9ecb4e4b768397ec4090add605f
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-05 07:03:20 +1000

    Fixed https://bugzilla.redhat.com/show_bug.cgi?id=923012

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306031528";
+    public static final String VERSION = "201306050654";
 
     /**
      * The maximum length of the title before it is truncated
