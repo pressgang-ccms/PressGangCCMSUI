2ef65115c64b5683533ae7ac508e739d24262184
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-05 14:08:42 +1000

    Added snippets. Worked out that spaces instead of tabs was causing issues.

--- src/main/webapp/javascript/acesnippets/snippets.js
+++ src/main/webapp/javascript/acesnippets/snippets.js
@@ -770,27 +770,27 @@ snippet figure\n\
 	<figure>\n\
 		<title>${1:title}</title>\n\
 		<mediaobject>\n\
-		    <imageobject>\n\
-			   <imagedata align='center' fileref='images/${2:imageid}.png'/>\n\
-		    </imageobject>\n\
-		    <textobject>\n\
-			   <phrase>${3:description}</phrase>\n\
-		    </textobject>\n\
+			<imageobject>\n\
+				<imagedata align='center' fileref='images/${2:imageid}.png'/>\n\
+			</imageobject>\n\
+			<textobject>\n\
+				<phrase>${3:description}</phrase>\n\
+			</textobject>\n\
 		</mediaobject>\n\
 	</figure>\n\
 snippet formalpara\n\
 	<formalpara>\n\
 		<title>${1:title}</title>\n\
 		<para>\n\
-		    ${2:para}\n\
+			${2:para}\n\
 		</para>\n\
 	</formalpara>\n\
 snippet itemizedlist\n\
 	<itemizedlist> \n\
-		    <listitem>\n\
-		    <para>\n\
-			  ${1:para}\n\
-		    </para>\n\
+		<listitem>\n\
+			<para>\n\
+				${1:para}\n\
+			</para>\n\
 		</listitem>\n\
 	</itemizedlist>\n\
 snippet keycombo\n\
@@ -801,7 +801,7 @@ snippet keycombo\n\
 snippet listitem\n\
 	<listitem>\n\
 		<para>\n\
-		    ${1:para}\n\
+			${1:para}\n\
 		</para>\n\
 	</listitem>\n\
 snippet menuchoice\n\
@@ -809,24 +809,24 @@ snippet menuchoice\n\
 snippet note\n\
 	<note>\n\
 		<para>\n\
-		    ${1:para}\n\
+			${1:para}\n\
 		</para>\n\
 	</note>\n\
 snippet orderedlist\n\
 	<orderedlist>\n\
 		<listitem>\n\
-		    <para>\n\
-			   ${1:para}\n\
-		    </para>\n\
+			<para>\n\
+				${1:para}\n\
+			</para>\n\
 		</listitem>\n\
 	</orderedlist>\n\
 snippet procedure\n\
 	<procedure>\n\
 		<title>${1:title}</title>\n\
 		<step>\n\
-		    <para>\n\
-			   ${2:para}\n\
-		    </para>\n\
+			<para>\n\
+				${2:para}\n\
+			</para>\n\
 		</step>\n\
 	</procedure>\n\
 snippet programlisting\n\
@@ -837,37 +837,37 @@ snippet section\n\
 	<section>\n\
 		<title>${1:title}</title>\n\
 		<para>\n\
-		    ${2:para}\n\
+			${2:para}\n\
 		</para>\n\
 	</section>\n\
 snippet step\n\
 	<step>\n\
 		<para>\n\
-		    ${1:para}\n\
+			${1:para}\n\
 		</para>\n\
 	</step>\n\
 snippet table\n\
 	<table frame='all'>\n\
 		<title>${1:title}</title>\n\
 		<tgroup cols='2' align='left' colsep='1' rowsep='1'>\n\
-		    <colspec colname='c1'/>\n\
-		    <colspec colname='c2'/>\n\
-		    <thead>\n\
-			   <row>\n\
+			<colspec colname='c1'/>\n\
+			<colspec colname='c2'/>\n\
+			<thead>\n\
+				<row>\n\
 					<entry>Header Row 0 Col 0</entry>\n\
 					<entry>Header Row 0 Col 1</entry>\n\
-			   </row>\n\
-		    </thead>\n\
-		    <tbody>\n\
-			   <row>\n\
+				</row>\n\
+			</thead>\n\
+			<tbody>\n\
+				<row>\n\
 					<entry>Row 1 Col 0</entry>\n\
 					<entry>Row 1 Col 1</entry>\n\
-			   </row>\n\
-			   <row>\n\
+				</row>\n\
+				<row>\n\
 					<entry>Row 2 Col 0</entry>\n\
 					<entry>Row 2 Col 1</entry> \n\
-			   </row>\n\
-		    </tbody>\n\
+				</row>\n\
+			</tbody>\n\
 		</tgroup>\n\
 	</table>\n\
 snippet ulink\n\
@@ -876,25 +876,25 @@ snippet variablelist\n\
 	<variablelist>\n\
 		<title>Title</title>\n\
 		<varlistentry>\n\
-		    <term>\n\
-			   ${1:term}\n\
-		    </term>\n\
-		    <listitem>\n\
-			   <para>\n\
+			<term>\n\
+				${1:term}\n\
+			</term>\n\
+			<listitem>\n\
+				<para>\n\
 					${2:para}\n\
-			   </para>\n\
-		    </listitem>\n\
+				</para>\n\
+			</listitem>\n\
 		</varlistentry>\n\
 	</variablelist>\n\
 snippet variablelistentry\n\
 	<varlistentry>\n\
 		<term>\n\
-		    ${1:term}\n\
+			${1:term}\n\
 		</term>\n\
 		<listitem>\n\
-		    <para>\n\
-			   ${2:para}\n\
-		    </para>\n\
+			<para>\n\
+				${2:para}\n\
+			</para>\n\
 		</listitem>\n\
 	</varlistentry>\n\
 snippet xref\n\
