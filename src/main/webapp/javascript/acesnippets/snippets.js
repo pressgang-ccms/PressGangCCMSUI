120ef0015018e2b8b58b71a61d9b349da15d9402
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-05 15:34:06 +1000

    Added a bunch of snippets from https://github.com/ddri/Sublime-Text-DocBook-XML-Snippets

--- src/main/webapp/javascript/acesnippets/snippets.js
+++ src/main/webapp/javascript/acesnippets/snippets.js
@@ -833,6 +833,15 @@ snippet programlisting\n\
 	<programlisting><![CDATA[\n\
 	${1:code}\n\
 	]]></programlisting>\n\
+snippet programlistingjava\n\
+	<programlisting language='Java' role='JAVA'><![CDATA[\n\
+	${1:code}\n\
+	]]></programlisting>\n\
+snippet programlistingxml\n\
+	<programlisting language='XML' role='XML'><![CDATA[\n\
+	${1:code}\n\
+	]]\n\
+	</programlisting>\n\
 snippet section\n\
 	<section>\n\
 		<title>${1:title}</title>\n\
@@ -866,6 +875,102 @@ snippet table\n\
 				<row>\n\
 					<entry>Row 2 Col 0</entry>\n\
 					<entry>Row 2 Col 1</entry> \n\
+				</row>\n\
+			</tbody>\n\
+		</tgroup>\n\
+	</table>\n\
+snippet table3\n\
+	<table frame='all'>\n\
+		<title>${1:title}</title>\n\
+		<tgroup cols='3' align='left' colsep='1' rowsep='1'>\n\
+			<colspec colname='c1'/>\n\
+			<colspec colname='c2'/>\n\
+			<colspec colname='c3'/>\n\
+			<thead>\n\
+				<row>\n\
+					<entry>Header Row 0 Col 0</entry>\n\
+					<entry>Header Row 0 Col 1</entry>\n\
+					<entry>Header Row 0 Col 2</entry>\n\
+				</row>\n\
+			</thead>\n\
+			<tbody>\n\
+				<row>\n\
+					<entry>Row 1 Col 0</entry>\n\
+					<entry>Row 1 Col 1</entry>\n\
+					<entry>Row 1 Col 2</entry>\n\
+				</row>\n\
+				<row>\n\
+					<entry>Row 2 Col 0</entry>\n\
+					<entry>Row 2 Col 1</entry> \n\
+					<entry>Row 2 Col 2</entry> \n\
+				</row>\n\
+			</tbody>\n\
+		</tgroup>\n\
+	</table>\n\
+snippet table4\n\
+	<table frame='all'>\n\
+		<title>${1:title}</title>\n\
+		<tgroup cols='4' align='left' colsep='1' rowsep='1'>\n\
+			<colspec colname='c1'/>\n\
+			<colspec colname='c2'/>\n\
+			<colspec colname='c3'/>\n\
+			<colspec colname='c4'/>\n\
+			<thead>\n\
+				<row>\n\
+					<entry>Header Row 0 Col 0</entry>\n\
+					<entry>Header Row 0 Col 1</entry>\n\
+					<entry>Header Row 0 Col 2</entry>\n\
+					<entry>Header Row 0 Col 3</entry>\n\
+				</row>\n\
+			</thead>\n\
+			<tbody>\n\
+				<row>\n\
+					<entry>Row 1 Col 0</entry>\n\
+					<entry>Row 1 Col 1</entry>\n\
+					<entry>Row 1 Col 2</entry>\n\
+					<entry>Row 1 Col 3</entry>\n\
+				</row>\n\
+				<row>\n\
+					<entry>Row 2 Col 0</entry>\n\
+					<entry>Row 2 Col 1</entry> \n\
+					<entry>Row 2 Col 2</entry> \n\
+					<entry>Row 2 Col 3</entry> \n\
+				</row>\n\
+			</tbody>\n\
+		</tgroup>\n\
+	</table>\n\
+snippet table5\n\
+	<table frame='all'>\n\
+		<title>${1:title}</title>\n\
+		<tgroup cols='5' align='left' colsep='1' rowsep='1'>\n\
+			<colspec colname='c1'/>\n\
+			<colspec colname='c2'/>\n\
+			<colspec colname='c3'/>\n\
+			<colspec colname='c4'/>\n\
+			<colspec colname='c5'/>\n\
+			<thead>\n\
+				<row>\n\
+					<entry>Header Row 0 Col 0</entry>\n\
+					<entry>Header Row 0 Col 1</entry>\n\
+					<entry>Header Row 0 Col 2</entry>\n\
+					<entry>Header Row 0 Col 3</entry>\n\
+					<entry>Header Row 0 Col 4</entry>\n\
+				</row>\n\
+			</thead>\n\
+			<tbody>\n\
+				<row>\n\
+					<entry>Row 1 Col 0</entry>\n\
+					<entry>Row 1 Col 1</entry>\n\
+					<entry>Row 1 Col 2</entry>\n\
+					<entry>Row 1 Col 3</entry>\n\
+					<entry>Row 1 Col 4</entry>\n\
+				</row>\n\
+				<row>\n\
+					<entry>Row 2 Col 0</entry>\n\
+					<entry>Row 2 Col 1</entry> \n\
+					<entry>Row 2 Col 2</entry> \n\
+					<entry>Row 2 Col 3</entry> \n\
+					<entry>Row 2 Col 4</entry> \n\
 				</row>\n\
 			</tbody>\n\
 		</tgroup>\n\
@@ -911,6 +1016,58 @@ snippet warning\n\
 			${1:para}\n\
 		</para>\n\
 	</warning>\n\
+snippet footnote\n\
+	<footnote>\n\
+		<para>\n\
+			${1:para}\n\
+		</para>\n\
+	</footnote>\n\
+snippet indexterm1\n\
+	<indexterm id='${1:ID}'><primary>${2:primary}</primary></indexterm>\n\
+snippet indexterm2\n\
+	<indexterm id='${1:ID}'><primary>${2:primary}</primary><secondary>${3:secondary}</secondary></indexterm>\n\
+snippet indexterm3\n\
+	<indexterm id='${1:ID}'><primary>${2:primary}</primary><secondary>${3:secondary}</secondary><tertiary>${4:tertiary}</tertiary>\n\
+snippet injectimage\n\
+	<!-- InjectImage: ${1:IMAGE_ID} -->\n\
+snippet injectlist\n\
+	<!-- InjectList: ${1:TOPIC_ID_COMMA_SEPARATED} -->\n\
+snippet injectalphasort\n\
+	<!-- InjectListAlphaSort: ${1:TOPIC_ID_COMMA_SEPARATED} -->\n\
+snippet injectlistitems\n\
+	<!-- InjectListItems: ${1:TOPIC_ID_COMMA_SEPARATED} -->\n\
+snippet injectsequence\n\
+	<!-- InjectSequence: ${1:TOPIC_ID_COMMA_SEPARATED} -->\n\
+snippet injecttext\n\
+	<!-- InjectText: ${1:TOPIC_ID} -->\n\
+snippet injecttitle\n\
+	<!-- InjectTitle: ${1:TOPIC_ID} -->\n\
+snippet promptlinux\n\
+	<prompt>[user@host ${1:path}]\$</prompt> <userinput>${2:user_input}</userinput>\n\
+snippet promptwindows\n\
+	<prompt>C:\${1:path}&gt;</prompt> <userinput>${2:user_input}</userinput>\n\
+snippet screen\n\
+	<screen><![CDATA[${1:screen_output}]]${2:>}</screen>\n\
+snippet stepalternatives\n\
+	<stepalternatives>\n\
+		<step>\n\
+			<title>${1:text}</title>\n\
+			<para>\n\
+				${2:text}\n\
+			</para>\n\
+		</step>\n\
+	</stepalternatives>\n\
+snippet substeps\n\
+	<substeps>\n\
+		<step>\n\
+			<title>${1:title}</title>\n\
+			<para>\n\
+				${2:text}\n\
+			</para>\n\
+		</step>\n\
+	</substeps>\n\
+snippet xiinclude\n\
+	<xi:include xmlns:xi='http://www.w3.org/2001/XInclude'' href='${1:path_to_file}'/>\n\
 ";
 });
 
