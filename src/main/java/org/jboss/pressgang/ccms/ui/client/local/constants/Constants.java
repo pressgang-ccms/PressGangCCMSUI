8a63aa42fdc1508624e415d369835a914959b45e
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-06 12:04:50 +1000

    Added negative words

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306061144";
+    public static final String VERSION = "201306061153";
 
     /**
      * The maximum length of the title before it is truncated
