6efdfe387e9d7d7de344f73606c9a1d4e8e8c0a6
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-06 13:32:04 +1000

    Added words to dictionaries

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306061310";
+    public static final String VERSION = "201306061326";
 
     /**
      * The maximum length of the title before it is truncated
