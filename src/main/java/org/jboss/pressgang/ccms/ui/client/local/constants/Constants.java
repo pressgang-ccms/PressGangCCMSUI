adc8a5c46c366e2af628c379e1a4bc8bf12cd58f
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-07 14:08:12 +1000

    Working on spell checking suggestions

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306070957";
+    public static final String VERSION = "201306071407";
 
     /**
      * The maximum length of the title before it is truncated
