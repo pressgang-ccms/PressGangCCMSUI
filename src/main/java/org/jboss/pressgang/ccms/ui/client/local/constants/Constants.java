91692db02f251304e7a1f9e390dd2690abd0bee2
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-09 08:58:47 +1000

    Updated spellchecking to provide suggestions for negative words, and also to search for hyphenated words in suggestions

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306071529";
+    public static final String VERSION = "201306090857";
 
     /**
      * The maximum length of the title before it is truncated
