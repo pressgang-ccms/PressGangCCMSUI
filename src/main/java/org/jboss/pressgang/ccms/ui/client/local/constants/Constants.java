0b9540b3cf22620caeb5a35fc4e99ce8c7faeb7e
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-10 13:00:38 +1000

    Recompiled to incorporate context menu loading twice fix.

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306090857";
+    public static final String VERSION = "201306101241";
 
     /**
      * The maximum length of the title before it is truncated
