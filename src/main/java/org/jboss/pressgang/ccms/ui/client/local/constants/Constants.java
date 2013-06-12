5f2cafb6f3929e0286ec0c23689906a8b64bd52c
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-12 15:01:15 +1000

    Worked on dictionary

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306121446";
+    public static final String VERSION = "201306121457";
 
     /**
      * XML Mime type
