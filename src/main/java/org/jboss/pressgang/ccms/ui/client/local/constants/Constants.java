5694e808a3327a40298ca985c035ef28c5bfc235
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-12 13:44:09 +1000

    Added a bunch of negative terms

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306121214";
+    public static final String VERSION = "201306121321";
 
     /**
      * XML Mime type
