bc8bb84f766960f2519e914b5db0d1c668c95bd3
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-25 08:12:24 +1000

    Applying fixed from Development branch

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306250758";
+    public static final String VERSION = "201306250812";
 
     /**
      * XML Mime type
