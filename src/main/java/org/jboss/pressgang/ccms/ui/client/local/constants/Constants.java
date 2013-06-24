56ab72cd6c07880bd06f060ff158831d1faf925e
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-25 08:00:07 +1000

    Fixed https://bugzilla.redhat.com/show_bug.cgi?id=977222

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306250740";
+    public static final String VERSION = "201306250758";
 
     /**
      * XML Mime type
