eff81d2436d4cdbfa029ced972569a2018a42480
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-25 10:58:06 +1000

    Fixed up memory leaks

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306251030";
+    public static final String VERSION = "201306251051";
 
     /**
      * XML Mime type
