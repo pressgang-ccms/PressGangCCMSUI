8e0082353dad58ccb8cdeed552c0dcb664d95852
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-26 12:51:29 +1000

    Updated marshalling list

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306261227";
+    public static final String VERSION = "201306261249";
 
     /**
      * XML Mime type
