3f25ef405b811433808e1e7d805906a964755200
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-27 13:30:55 +1000

    Worked around some browser incompatibility issues

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306271304";
+    public static final String VERSION = "201306271316";
 
     /**
      * XML Mime type
