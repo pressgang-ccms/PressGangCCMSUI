f15b93e8b4c36694d564b03d6fe30e29b542c778
Matthew Casperson <matthewcasperson@gmail.com>
2013-06-27 15:03:09 +1000

    /me shakes fish at Firefox! Why are you so slow?

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201306271426";
+    public static final String VERSION = "201306271452";
 
     /**
      * XML Mime type
