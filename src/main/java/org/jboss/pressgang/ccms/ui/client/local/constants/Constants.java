e0899c4ca3309c2a7aab1e03dc35b5b4874b7a6f
Matthew Casperson <matthewcasperson@gmail.com>
2013-07-15 11:43:26 +1000

    Updated failover logic for situations where there are a number of concurrent requests.

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201307150826";
+    public static final String VERSION = "201307150838";
 
     /**
      * The JSON Key for a recently failed failed server id
