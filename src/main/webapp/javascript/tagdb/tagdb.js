a05f117a5654cfb51332bb8c84905ca6ece4dbf0
Matthew Casperson <matthewcasperson@gmail.com>
2013-07-16 17:49:05 +1000

    Working on adding tag sytle guides

--- src/main/webapp/javascript/tagdb/tagdb.js
+++ src/main/webapp/javascript/tagdb/tagdb.js
@@ -6,14 +6,17 @@ self.addEventListener('message', function (e) {
         for (var lineIndex = 0, linesLength = lines.length; lineIndex < linesLength; ++lineIndex) {
 
             var line = lines[lineIndex];
+            var tags = [];
 
             // match only xml/html elements
             var tagRe = /<\s*([^\s]+).*?>/g;
             var tagMatch = null;
             while (tagMatch = tagRe.exec(line)) {
                 var tag = tagMatch[1];
-                retValue.push([tagMatch.index, tag.length]);
+                tags.push([tagMatch.index, tag.length]);
             }
+
+            retValue.push(tags);
         }
 
         postMessage(retValue);
