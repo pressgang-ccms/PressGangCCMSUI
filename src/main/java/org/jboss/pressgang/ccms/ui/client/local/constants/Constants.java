f31bb3218f9502cf437a4714c29826f24b01a0e6
Matthew Casperson <matthewcasperson@gmail.com>
2013-07-16 17:51:55 +1000

    Working on adding tag sytle guides

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201307161747";
+    public static final String VERSION = "201307161749";
 
     /**
      * The JSON Key for a recently failed failed server id
