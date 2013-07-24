89f2db14b245769be59de3326817c1481462772d
Matthew Casperson <matthewcasperson@gmail.com>
2013-07-25 08:59:48 +1000

    Fixed help overlay style

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201307250833";
+    public static final String VERSION = "201307250838";
 
     /**
      * The height of the tabs in a tab panel, in EMs
