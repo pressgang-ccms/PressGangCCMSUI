7a80a5fb243a965432d9f82a3f51d87ebcc4e229
Matthew Casperson <matthewcasperson@gmail.com>
2013-07-25 14:46:38 +1000

    Fixed xml error line numbers with entities being prepended to the validated XML

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/mvp/presenter/topic/searchresults/topics/TopicFilteredResultsAndDetailsPresenter.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/mvp/presenter/topic/searchresults/topics/TopicFilteredResultsAndDetailsPresenter.java
@@ -1662,10 +1662,13 @@ public class TopicFilteredResultsAndDetailsPresenter extends BaseTopicFilteredRe
 	}-*/;
 
     private native void checkXML() /*-{
-		var displayComponent = this.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.searchresults.topics.TopicFilteredResultsAndDetailsPresenter::getTopicXMLPresenter()();
+        var displayComponent = this.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.searchresults.topics.TopicFilteredResultsAndDetailsPresenter::getTopicXMLPresenter()();
 		var display = displayComponent.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.TopicXMLPresenter::getDisplay()();
 
-		if (this.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.searchresults.topics.TopicFilteredResultsAndDetailsPresenter::worker == null) {
+		var entities = @org.jboss.pressgang.ccms.ui.client.local.data.DocbookDTD::getDtdDoctype()();
+
+
+        if (this.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.searchresults.topics.TopicFilteredResultsAndDetailsPresenter::worker == null) {
 			this.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.searchresults.topics.TopicFilteredResultsAndDetailsPresenter::worker = new Worker('javascript/xmllint/xmllint.js');
 			this.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.searchresults.topics.TopicFilteredResultsAndDetailsPresenter::worker.addEventListener('message', function (me) {
 				return function (e) {
@@ -1680,21 +1683,21 @@ public class TopicFilteredResultsAndDetailsPresenter extends BaseTopicFilteredRe
                     if (theseErrors == "" && oldErrors == "") {
 						errors.@com.google.gwt.user.client.ui.TextArea::setText(Ljava/lang/String;)(noXmlErrors);
                     } else if (oldErrors != theseErrors) {
-						// "Document topic.xml does not validate against docbook45.dtd" is a standard part of the error
+						var entitiesLines = entities.indexOf("\n") == -1 ? 0 : entities.match(/\n/g).length;
+
+                        // "Document topic.xml does not validate against docbook45.dtd" is a standard part of the error
 						// message, and is removed before being displayed.
 						var errorMessage = theseErrors.replace("\nDocument topic.xml does not validate against docbook45.dtd", "");
-						if (errorMessage.length == 0) {
-							errors.@com.google.gwt.user.client.ui.TextArea::setText(Ljava/lang/String;)(noXmlErrors);
-						} else {
-							errors.@com.google.gwt.user.client.ui.TextArea::setText(Ljava/lang/String;)(errorMessage);
-						}
 
-						var errorLineRegex = /topic.xml:(\d+):/g;
+						var errorLineRegex = /topic\.xml:(\d+):/g;
 						var match = null;
 						var lineNumbers = [];
 						while (match = errorLineRegex.exec(theseErrors)) {
 							if (match.length >= 1) {
-								var line = parseInt(match[1]) - 1;
+                                var line = parseInt(match[1]) - entitiesLines - 1;
+								// We need to match the line numbers in the editor with those in the topic, which
+                                // means subtracting all the entities we added during validation.
+								errorMessage = errorMessage.replace("topic.xml:" + match[1], "topic.xml:" + line);
 								var found = false;
 								for (var i = 0, lineNumbersLength = lineNumbers.length; i < lineNumbersLength; ++i) {
 									if (lineNumbers[i] == line) {
@@ -1706,6 +1709,12 @@ public class TopicFilteredResultsAndDetailsPresenter extends BaseTopicFilteredRe
 									lineNumbers.push(line);
 								}
 							}
+						}
+
+						if (errorMessage.length == 0) {
+							errors.@com.google.gwt.user.client.ui.TextArea::setText(Ljava/lang/String;)(noXmlErrors);
+						} else {
+							errors.@com.google.gwt.user.client.ui.TextArea::setText(Ljava/lang/String;)(errorMessage);
 						}
 
 						if (editor != null) {
@@ -1724,8 +1733,7 @@ public class TopicFilteredResultsAndDetailsPresenter extends BaseTopicFilteredRe
 			if (editor != null) {
                 var text = editor.@edu.ycp.cs.dh.acegwt.client.ace.AceEditor::getText()();
                 // Add the doctype that include the standard docbook entities
-                text = @org.jboss.pressgang.ccms.ui.client.local.data.DocbookDTD::getDtdDoctype()() +
-                    @org.jboss.pressgang.ccms.ui.client.local.utilities.GWTUtilities::removeAllPreabmle(Ljava/lang/String;)(text);
+                text = entities + @org.jboss.pressgang.ccms.ui.client.local.utilities.GWTUtilities::removeAllPreabmle(Ljava/lang/String;)(text);
                 if (text == this.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.searchresults.topics.TopicFilteredResultsAndDetailsPresenter::worker.lastXML) {
                     this.@org.jboss.pressgang.ccms.ui.client.local.mvp.presenter.topic.searchresults.topics.TopicFilteredResultsAndDetailsPresenter::timeout = $wnd.setTimeout(function(me) {
                         return function(){
@@ -2962,7 +2970,7 @@ public class TopicFilteredResultsAndDetailsPresenter extends BaseTopicFilteredRe
         /*
             Property ui elements
         */
-        addHelpDataToMap(this.helpDatabase, new HelpData(topicViewPresenter.getDisplay().getEditor().titleEditor(), ServiceConstants.HELP_TOPICS.TOPIC_PROPERT_TITLE.getId(), 7));
+        addHelpDataToMap(this.helpDatabase, new HelpData(topicViewPresenter.getDisplay().getEditor().titleEditor(), ServiceConstants.HELP_TOPICS.TOPIC_PROPERTY_TITLE.getId(), 7));
         addHelpDataToMap(this.helpDatabase, new HelpData(topicViewPresenter.getDisplay().getEditor().getRestTopicDetails(), ServiceConstants.HELP_TOPICS.TOPIC_PROPERTY_REST_ENDPOINT.getId(), 7));
         addHelpDataToMap(this.helpDatabase, new HelpData(topicViewPresenter.getDisplay().getEditor().getRestTopicXML(), ServiceConstants.HELP_TOPICS.TOPIC_PROPERTY_REST_XML_ENDPOINT.getId(), 7));
         addHelpDataToMap(this.helpDatabase, new HelpData(topicViewPresenter.getDisplay().getEditor().getRestTopicWebDav(), ServiceConstants.HELP_TOPICS.TOPIC_PROPERTY_WEBDAV_URL.getId(), 7));
