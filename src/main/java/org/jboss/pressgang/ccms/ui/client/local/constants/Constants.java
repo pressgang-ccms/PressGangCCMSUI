a47d86438a67d01d79c7c20be9b3a899eae2f2fe
Lee Newson <lnewson@redhat.com>
2013-08-23 20:34:00 +1000

    Updated the Welcome and DocBuilder views to hide the top action bar.

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201308231838";
+    public static final String VERSION = "201308231901";
 
     /**
      * The attribute that is used by the help overlay system
