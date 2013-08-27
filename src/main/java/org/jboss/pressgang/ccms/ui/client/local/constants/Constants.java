12a50db90c864648e9fe62a0f6715bf49bb6df01
Matthew Casperson <matthewcasperson@gmail.com>
2013-08-27 10:42:13 +1000

    Fixed show/hide search results button

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201308131736";
+    public static final String VERSION = "201308131751";
 
     /**
      * The attribute that is used by the help overlay system
