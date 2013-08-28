e13344aad196c5826448adcd04049cdfa5b57b01
Matthew Casperson <matthewcasperson@gmail.com>
2013-08-28 11:39:18 +1000

    Fixed ACE style for content specs

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201308280951";
+    public static final String VERSION = "201308281139";
 
     /**
      * Monospaced web safe fonts
--- src/main/webapp/javascript/ace/mode-csp.js
+++ src/main/webapp/javascript/ace/mode-csp.js
@@ -1,49 +1,54 @@
-define("ace/mode/csp", ["require","exports","module","ace/lib/oop","ace/mode/text","ace/tokenizer", "ace/mode/csp_highlighter"], function(require, exports, module) {
-	"use strict";
-	var oop = require("../lib/oop");
-	// defines the parent mode
-	var TextMode = require("./text").Mode;
-	var Tokenizer = require("../tokenizer").Tokenizer;
-	var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent;
-	// defines the language specific highlighters and folding rules
-	var MyNewHighlightRules = require("./csp_highlighter").MyNewHighlightRules;
-	//var MyNewFoldMode = require("./folding/mynew").MyNewFoldMode;
-	var Mode = function() {
-		// set everything up
-		var highlighter = new MyNewHighlightRules();
-		this.$outdent = new MatchingBraceOutdent();
-		//this.foldingRules = new MyNewFoldMode();
-		this.$tokenizer = new Tokenizer(highlighter.getRules());
-	};
-	oop.inherits(Mode, TextMode);
-	(function() {
-		// Extra logic goes here--we won't be covering all of this
-		/* These are all optional pieces of code!
-		 this.getNextLineIndent = function(state, line, tab) {
-		 var indent = this.$getIndent(line);
-		 return indent;
-		 };
-		 this.checkOutdent = function(state, line, input) {
-		 return this.$outdent.checkOutdent(line, input);
-		 };
-		 this.autoOutdent = function(state, doc, row) {
-		 this.$outdent.autoOutdent(doc, row);
-		 };
-		 this.createWorker = function(session) {
-		 var worker = new WorkerClient(["ace"], "ace/mode/mynew_worker", "NewWorker");
-		 worker.attachToDocument(session.getDocument());
-		 return worker;
-		 };
-		 */
-	}).call(Mode.prototype);
+define(
+	"ace/mode/csp",
+	["require","exports","module","ace/lib/oop","ace/mode/text","ace/tokenizer", "ace/mode/csp_highlighter", "ace/mode/matching_brace_outdent", "ace/mode/folding/csp_style"],
+	function(require, exports, module) {
+		"use strict";
+		var oop = require("../lib/oop");
+		// defines the parent mode
+		var TextMode = require("./text").Mode;
+		var Tokenizer = require("../tokenizer").Tokenizer;
+		var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent;
+		// defines the language specific highlighters and folding rules
+		var MyNewHighlightRules = require("./csp_highlighter").HighlightRules;
+		//var MyNewFoldMode = require("./folding/csp_style").FoldMode;
+		var Mode = function() {
+			// set everything up
+			var highlighter = new MyNewHighlightRules();
+			this.$outdent = new MatchingBraceOutdent();
+			//this.foldingRules = new MyNewFoldMode();
+			this.$tokenizer = new Tokenizer(highlighter.getRules());
+		};
+		oop.inherits(Mode, TextMode);
+		(function() {
+			// Extra logic goes here--we won't be covering all of this
+			/* These are all optional pieces of code!
+			 this.getNextLineIndent = function(state, line, tab) {
+			 var indent = this.$getIndent(line);
+			 return indent;
+			 };
+			 this.checkOutdent = function(state, line, input) {
+			 return this.$outdent.checkOutdent(line, input);
+			 };
+			 this.autoOutdent = function(state, doc, row) {
+			 this.$outdent.autoOutdent(doc, row);
+			 };
+			 this.createWorker = function(session) {
+			 var worker = new WorkerClient(["ace"], "ace/mode/mynew_worker", "NewWorker");
+			 worker.attachToDocument(session.getDocument());
+			 return worker;
+			 };
+			 */
+		}).call(Mode.prototype);
 	exports.Mode = Mode;
 });
 
-define("ace/mode/csp_highlighter", ["require","exports","module","ace/lib/oop","ace/mode/text","ace/tokenizer"], function(require, exports, module) {
+define(
+	"ace/mode/csp_highlighter",
+	["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
 	"use strict";
 	var oop = require("../lib/oop");
 	var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
-	var MyNewHighlightRules = function() {
+	var HighlightRules = function() {
 		// regexp must not have capturing parentheses. Use (?:) instead.
 		// regexps are ordered -> the first match is used
 		this.$rules = {
@@ -60,7 +65,7 @@ define("ace/mode/csp_highlighter", ["require","exports","module","ace/lib/oop","
 						"|^Book Version\\s*=|^Copyright\\s*=|^Output Style\\s*="
 				}, {
 					// tokens that define structure
-					token: "entity.name.section",
+					token: "constant.language",
 					regex: "^\\s*Chapter:|^\\s*Section:|^\\s*Appendix:|^\\s*Part:|^\\s*Procedure:|Refer-to:|Prerequisite:|T\\d+|R:\\d+|P:\\d+|\\bN\\d*\\b|X\\d+"
 				},  {
 					token : "constant.numeric",
@@ -79,6 +84,29 @@ define("ace/mode/csp_highlighter", ["require","exports","module","ace/lib/oop","
 			]
 		};
 	};
-	oop.inherits(MyNewHighlightRules, TextHighlightRules);
-	exports.MyNewHighlightRules = MyNewHighlightRules;
+	oop.inherits(HighlightRules, TextHighlightRules);
+	exports.HighlightRules = HighlightRules;
 });
+
+define(
+	"ace/mode/folding/csp_style",
+	["require","exports","module","ace/lib/oop","ace/range","ace/mode/folding/fold_mode"],
+	function(require, exports, module) {
+		"use strict";
+		var oop = require("../../lib/oop");
+		var Range = require("../../range").Range;
+		var BaseFoldMode = require("./fold_mode").FoldMode;
+		var FoldMode = exports.FoldMode = function() {};
+		oop.inherits(FoldMode, BaseFoldMode);
+		(function() {
+			// regular expressions that identify starting and stopping points
+			this.foldingStartMarker = //;
+			this.foldingStopMarker = //;
+			this.getFoldWidgetRange = function(session, foldStyle, row) {
+				var line = session.getLine(row);
+				// test each line, and return a range of segments to collapse
+			};
+		}).call(FoldMode.prototype);
+});
+
+define("ace/mode/matching_brace_outdent",["require","exports","module","ace/range"],function(e,t,n){var r=e("../range").Range,i=function(){};(function(){this.checkOutdent=function(e,t){return/^\s+$/.test(e)?/^\s*\}/.test(t):!1},this.autoOutdent=function(e,t){var n=e.getLine(t),i=n.match(/^(\s*\})/);if(!i)return 0;var s=i[1].length,o=e.findMatchingBracket({row:t,column:s});if(!o||o.row==t)return 0;var u=this.$getIndent(e.getLine(o.row));e.replace(new r(t,0,t,s-1),u)},this.$getIndent=function(e){return e.match(/^\s*/)[0]}}).call(i.prototype),t.MatchingBraceOutdent=i});
