fb8962270aa325198044eddd7f267bf7ce38b605
Matthew Casperson <matthewcasperson@gmail.com>
2013-09-02 07:31:35 +1000

    Fixed filenames

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201309011116";
+    public static final String VERSION = "201309020727";
 
     /**
      * Monospaced web safe fonts
