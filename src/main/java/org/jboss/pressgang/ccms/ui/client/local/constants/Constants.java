4cb2e0e17a13f3cf19755584256eaae3313d9916
Matthew Casperson <matthewcasperson@gmail.com>
2013-09-08 07:40:05 +1000

    Fixed conditional rendering, using the default condition when none is specified.

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201309041223";
+    public static final String VERSION = "201309041310";
 
     /**
      * Monospaced web safe fonts
