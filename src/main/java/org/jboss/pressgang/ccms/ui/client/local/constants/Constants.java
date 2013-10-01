68b4415f76a5217dcc3657a98fa6cc6ca5d6b1e7
Matthew Casperson <mcaspers@redhat.com>
2013-10-01 16:48:27 +1000

    Updated help overlay

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201309271553";
+    public static final String VERSION = "201310011644";
 
     /**
      * Monospaced web safe fonts
