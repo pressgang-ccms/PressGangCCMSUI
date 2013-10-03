0f5a929d7b942d1b02a1a23ce66b2433400e469e
Matthew Casperson <matthewcasperson@gmail.com>
2013-10-03 14:05:56 +1000

    Working on https://bugzilla.redhat.com/show_bug.cgi?id=1012761

---
+++ src/main/webapp/javascript/tagdb/contentSpecTagDB.js
@@ -0,0 +1,45 @@
+/*
+ This script scans an array of lines of text looking for content spec metadata elements
+ */
+
+var tags = null;
+
+self.addEventListener('message', function (e) {
+	if (e.data.tagDB) {
+		var database = JSON.parse(e.data.tagDB);
+		tags = [];
+
+		for (var property in database) {
+			if (database.hasOwnProperty(property)) {
+				tags.push(property);
+			}
+		}
+	}
+
+	if (e.data.lines && tags != null) {
+		var lines = e.data.lines;
+
+		var retValue = [];
+		for (var lineIndex = 0, linesLength = lines.length; lineIndex < linesLength; ++lineIndex) {
+
+			var line = lines[lineIndex];
+			var tagDetails = [];
+
+			// Match anything that looks like metadata
+			var metadataRe = /^(\S*)\s*=\s*/g;
+			var metadataMatch = null;
+			while (metadataMatch = metadataRe.exec(line)) {
+				var tag = metadataMatch[1];
+
+				if (tags.indexOf(tag) != -1) {
+					var prefix = metadataMatch[1];
+					var start = metadataMatch.index + prefix.length;
+					var end = start + tag.length;
+					tagDetails.push([start, end]);
+				}
+			}
+		}
+
+		postMessage(retValue);
+	}
+});
