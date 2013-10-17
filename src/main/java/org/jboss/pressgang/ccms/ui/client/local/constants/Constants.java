75c20c06ab64ec500e02f58cb8059e341524e8a2
Matthew Casperson <mcaspers@redhat.com>
2013-10-17 12:44:41 +1000

    Updated ACE editor to package 10.07.2013

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201310171231";
+    public static final String VERSION = "201310171244";
 
     /**
      * The name of the DocBook title element
