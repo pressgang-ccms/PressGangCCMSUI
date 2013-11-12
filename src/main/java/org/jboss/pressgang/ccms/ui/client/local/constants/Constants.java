33c7fed4c6d50a1f6d754944c1d5691ef4540833
Matthew Casperson <mcaspers@redhat.com>
2013-11-12 15:03:57 +1000

    Fixed https://bugzilla.redhat.com/show_bug.cgi?id=1015327

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
     /**
      * The UI Version - yyyymmddhhmm.
      */
-    public static final String VERSION = "201310291225";
+    public static final String VERSION = "201311121459";
 
     /**
      * The name of the DocBook title element
