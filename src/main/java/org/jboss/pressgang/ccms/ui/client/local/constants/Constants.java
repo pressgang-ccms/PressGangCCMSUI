9dfb20b7d2a89705ba50bbb4a929d9d7c0056129
Matthew Casperson <mcaspers@redhat.com>
2013-12-02 08:59:15 +1000

    Reverted changes to TopicRenderedPresenter

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
     /**
      * The UI Build - yyyymmddhhmm.
      */
-    public static final String BUILD = "201312020758";
+    public static final String BUILD = "201312020841";
 
     /**
      * A collection of all the Docbook 4.5 entities
