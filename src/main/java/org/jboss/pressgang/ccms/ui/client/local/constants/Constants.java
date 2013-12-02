3776512f5cc7087c302ca99b05ab53fde282a126
Matthew Casperson <mcaspers@redhat.com>
2013-12-02 18:27:13 +1000

    Fixing https://bugzilla.redhat.com/show_bug.cgi?id=1036397

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
     /**
      * The UI Build - yyyymmddhhmm.
      */
-    public static final String BUILD = "201312021303";
+    public static final String BUILD = "201312021314";
 
     /**
      * A collection of all the Docbook 4.5 entities
