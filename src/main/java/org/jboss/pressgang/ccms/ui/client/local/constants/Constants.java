1bdd85293967910f4278f66b693c27ac3efb9888
Matthew Casperson <mcaspers@redhat.com>
2013-12-03 14:06:03 +1000

    Fixed potential wrapping issue in the alert box with a bulk upload of files

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
     /**
      * The UI Build - yyyymmddhhmm.
      */
-    public static final String BUILD = "201312021849";
+    public static final String BUILD = "201312030748";
 
     /**
      * A collection of all the Docbook 4.5 entities
