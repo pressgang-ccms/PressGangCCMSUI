57469910daab837d62f8f285fbd27ca33008849f
Matthew Casperson <mcaspers@redhat.com>
2014-01-17 12:37:21 +1000

    Started work on https://bugzilla.redhat.com/show_bug.cgi?id=1035949

--- src/main/webapp/javascript/highlighters/conditional.js
+++ src/main/webapp/javascript/highlighters/conditional.js
@@ -1,4 +1,13 @@
+/*
+    This JavaScript takes xml and scans it for xml elements that do not match the given condition.
+    Since it uses regular expressions, it is somewhat tolerant of badly formatted XML.
+ */
+
 self.addEventListener('message', function (e) {
+    /**
+     * A regex to match an XML element with a condition attribute
+     * @type {RegExp}
+     */
     var startTagRegex = /<\s*([^>]*?)\s+[^>]*?condition\s*=\s*['"](.*?)['"][^>]*?(\/)?\s*>/;
     var text = e.data.text;
     var textConditions = e.data.condition.split(":;,");
@@ -41,7 +50,9 @@ self.addEventListener('message', function (e) {
 
             // not a self closing element
             if (!match[3]) {
+                // a regex to match any opening tag of the same type as the conditional one we are processing
                 var openTag = new RegExp("<\s*" + match[1] + "\s*.*?>");
+                // a regex to match any closing tag of the same type as the conditial one we are processing
                 var closeTag = new RegExp("<\s*\/\s*" + match[1] + "\s*>");
 
                 var childCount = 0;
@@ -56,10 +67,14 @@ self.addEventListener('message', function (e) {
                         retValue.push({start: start, end: totalLength + text.length});
                         break outerloop;
                     } else if (openMatch == null || openMatch.index > closeMatch.index) {
+                        // we found a closing tag without finding an opening tag, or
+                        // where the closing tag was before the next opening one
                         --childCount;
                         totalLength += closeMatch.index + closeMatch[0].length;
                         text = text.substr(closeMatch.index + closeMatch[0].length);
                     } else if (openMatch != null && openMatch.index < closeMatch.index) {
+                        // we found an opening tag before the next closing one. This
+                        // means the opening tag is a child of the conditional one
                         ++childCount;
                         totalLength += openMatch.index + openMatch[0].length;
                         text = text.substr(openMatch.index + openMatch[0].length);
