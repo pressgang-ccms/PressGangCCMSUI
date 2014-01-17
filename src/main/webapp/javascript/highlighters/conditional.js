50a36e85674143cd343d5b7baa6641c17c753fb8
Matthew Casperson <mcaspers@redhat.com>
2014-01-17 12:33:04 +1000

    Started work on https://bugzilla.redhat.com/show_bug.cgi?id=1035949

---
+++ src/main/webapp/javascript/highlighters/conditional.js
@@ -0,0 +1,77 @@
+self.addEventListener('message', function (e) {
+    var startTagRegex = /<\s*([^>]*?)\s+[^>]*?condition\s*=\s*['"](.*?)['"][^>]*?(\/)?\s*>/;
+    var text = e.data.text;
+    var textConditions = e.data.condition.split(":;,");
+    var conditions = [];
+    var totalLength = 0;
+
+    for (var textConditionIndex = 0, textConditionCount = textConditions.length; textConditionIndex < textConditionCount; ++textConditionIndex) {
+        var textCondition = textConditions[textConditionIndex];
+        try {
+            conditions.push(new RegExp(textCondition));
+        } catch (e) {
+            // bad regex, so ignore it
+        }
+    }
+
+    var retValue = [];
+
+    outerloop:
+    while (text.length != 0) {
+        var match = startTagRegex.exec(text);
+        if (match == null) {
+            break outerloop;
+        }
+
+        var include = false;
+        for (var conditionIndex = 0, conditionCount = conditions.length; conditionIndex < conditionCount; ++conditionIndex) {
+            var condition = conditions[conditionIndex];
+            if(condition.exec(match[2])) {
+                include = true;
+                break;
+            }
+        }
+
+        if (!include) {
+            var start = match.index + totalLength;
+
+            totalLength += match.index + match[0].length;
+
+            text = text.substr(match.index + match[0].length);
+
+            // not a self closing element
+            if (!match[3]) {
+                var openTag = new RegExp("<\s*" + match[1] + "\s*.*?>");
+                var closeTag = new RegExp("<\s*\/\s*" + match[1] + "\s*>");
+
+                var childCount = 0;
+
+                while (childCount >= 0) {
+                    var openMatch = openTag.exec(text);
+                    var closeMatch = closeTag.exec(text);
+
+                    if (closeMatch == null) {
+                        // no close tag exists. this is bad xml, but we will highlight it
+                        // like the open tag is spread to the end of the text
+                        retValue.push({start: start, end: totalLength + text.length});
+                        break outerloop;
+                    } else if (openMatch == null || openMatch.index > closeMatch.index) {
+                        --childCount;
+                        totalLength += closeMatch.index + closeMatch[0].length;
+                        text = text.substr(closeMatch.index + closeMatch[0].length);
+                    } else if (openMatch != null && openMatch.index < closeMatch.index) {
+                        ++childCount;
+                        totalLength += openMatch.index + openMatch[0].length;
+                        text = text.substr(openMatch.index + openMatch[0].length);
+                    }
+                }
+
+                retValue.push({start: start, end: totalLength});
+            } else {
+                retValue.push({start: start, end: totalLength});
+            }
+        }
+    }
+
+    postMessage(retValue);
+});
