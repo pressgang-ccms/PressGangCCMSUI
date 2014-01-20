615c0a6c23a2034a4a07cee76cbe151aeb960e0f
Matthew Casperson <mcaspers@redhat.com>
2014-01-20 18:07:15 +1000

    Fixed tag matching for conditional highlights

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
     /**
      * The UI Build - yyyymmddhhmm.
      */
-    public static final String BUILD = "201401201514";
+    public static final String BUILD = "201401201755";
 
     /**
      * A collection of all the Docbook 4.5 entities
