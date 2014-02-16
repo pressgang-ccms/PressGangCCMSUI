4f631c320f6ac89b5fc09a312487fadd90f99bff
Lee Newson <lnewson@redhat.com>
2014-02-16 19:08:30 +1000

    Updated ace to package 01.08.2014.

---
+++ src/main/webapp/javascript/ace/snippets/csp.js
@@ -0,0 +1,7 @@
+define('ace/snippets/csp', ['require', 'exports', 'module' ], function (require, exports, module) {
+
+    exports.snippetText = "snippet [N\n\
+	[N, ${1:Type}, writer=${2:Writer}]\n\
+";
+    exports.scope = "csp";
+});
