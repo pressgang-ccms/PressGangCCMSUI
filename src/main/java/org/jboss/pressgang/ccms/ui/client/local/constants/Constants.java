7a87b0cf3eed9a0b0d64dad5342a552093ad66d7
Matthew Casperson <matthewcasperson@gmail.com>
2014-05-05 16:08:13 +1000

    Fixing https://bugzilla.redhat.com/show_bug.cgi?id=1075865

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -20,7 +20,7 @@ public final class Constants {
     /**
      * The UI Build - yyyymmddhhmm.
      */
-    public static final String BUILD = "201405021025";
+    public static final String BUILD = "201405050825";
 
     public static final String DUMMY_CS_ENTITIES = "<!ENTITY PRODUCT \"Product\">\n" +
             "<!ENTITY BOOKID \"BOOKID\">\n" +
