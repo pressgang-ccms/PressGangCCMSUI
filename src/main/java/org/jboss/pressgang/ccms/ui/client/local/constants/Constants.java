3491bc0b10363afd6c8915c0ca0028fe90e58b8b
Lee Newson <lnewson@redhat.com>
2014-06-20 10:26:26 +1000

    Updated the Bugzilla link to 1.7

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -21,7 +21,7 @@ public final class Constants {
     /**
      * The UI Build - yyyymmddhhmm.
      */
-    public static final String BUILD = "201406180823";
+    public static final String BUILD = "201406180829";
 
     public static final String DUMMY_CS_ENTITIES = "<!ENTITY PRODUCT \"Product\">\n" +
             "<!ENTITY BOOKID \"BOOKID\">\n" +
@@ -1490,7 +1490,7 @@ public final class Constants {
      * The Bugzilla link for this product.
      */
     public static final String BUGZILLA_URL = BUGZILLA_BASE_URL + "/enter_bug.cgi?product=PressGang CCMS&component=Web-UI&version=1" +
-            ".6&cf_build_id=UI%20Build%20" + BUILD;
+            ".7&cf_build_id=UI%20Build%20" + BUILD;
     /**
      * A link to the survey.
      */
