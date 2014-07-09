c068484466dab743e1c1068dd5d0c8095242089b
Lee Newson <lnewson@redhat.com>
2014-07-10 09:43:12 +1000

    Updated the bugzilla link to 1.8

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -1483,7 +1483,7 @@ public final class Constants {
      * The Bugzilla link for this product.
      */
     public static final String BUGZILLA_URL = BUGZILLA_BASE_URL + "/enter_bug.cgi?product=PressGang CCMS&component=Web-UI&version=1" +
-            ".7&cf_build_id=UI%20Build%20" + BUILD;
+            ".8&cf_build_id=UI%20Build%20" + BUILD;
     /**
      * A link to the survey.
      */
