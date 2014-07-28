761ccf5991edaed315e33ebddb98b9247260cbbb
Matthew Casperson <matthewcasperson@gmail.com>
2014-07-28 12:40:49 +1000

    Added license info for LGPL v3

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/mvp/view/base/searchandedit/DisplaySplitViewCallback.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/mvp/view/base/searchandedit/DisplaySplitViewCallback.java
@@ -1,12 +1,24 @@
+/*
+  Copyright 2011-2014 Red Hat
+
+  This file is part of PresGang CCMS.
+
+  PresGang CCMS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  PresGang CCMS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with PresGang CCMS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.jboss.pressgang.ccms.ui.client.local.mvp.view.base.searchandedit;
 
-/**
- * Created with IntelliJ IDEA.
- * User: matthew
- * Date: 3/8/13
- * Time: 11:48 AM
- * To change this template use File | Settings | File Templates.
- */
 public interface DisplaySplitViewCallback {
     /**
      * When working with a split panel, all the panels added around the edge before
