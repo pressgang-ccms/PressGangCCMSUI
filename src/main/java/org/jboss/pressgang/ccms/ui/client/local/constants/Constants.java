ea434379b9d71a6185a7b0d036401a0c5f18e827
Lee Newson <lnewson@redhat.com>
2014-08-29 14:26:55 +1000

    PressGang 1.9 Release

--- src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
+++ src/main/java/org/jboss/pressgang/ccms/ui/client/local/constants/Constants.java
@@ -37,12 +37,12 @@ public final class Constants {
     /**
      * The UI version
      */
-    public static final String VERSION = "1.9-SNAPSHOT";
+    public static final String VERSION = "1.9";
 
     /**
      * The UI Build - yyyymmddhhmm.
      */
-    public static final String BUILD = "201408291005";
+    public static final String BUILD = "201408291425";
 
     public static final String DUMMY_CS_ENTITIES = "<!ENTITY PRODUCT \"Product\">\n" +
             "<!ENTITY BOOKID \"BOOKID\">\n" +
